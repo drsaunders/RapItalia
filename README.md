f60080649270c9552cb9da737d93b2294bb84415
Daniel Saunders <danielrsaunders@gmail.com>
2016-05-22 23:27:41 +0200

    Add proportional graphs, tweaks to look and wording

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 Proposal: RapItalia
 -------------------
 
-Living in Italy and observing how hip hop is a relatively new phenomenon here, I got interested in how coolness travels between cultures, with a delay. An understanding of this can be used to make money. My hypothesis was that Italians began to develop a taste for rap music in about the mid 2000s (about 10 years after it became a powerful force in North American pop culture). 
+Living in Italy and observing how hip hop is a relatively new phenomenon here, I got interested in how coolness travels between cultures, with a delay. Something that could turn out to be very profitable. My hypothesis was that Italians began to develop a taste for rap music in about the mid 2000s (about 10 years after it became a powerful force in North American pop culture). 
 
-To evaluate the popularity of rap in Italy over time, I used as a proxy the publication date of songs found on the Italian lyrics website "aztesti.it". Although the process used to select songs is unknown, such websites are designed to optimize traffic, and so they offer a sample of songs regularly searched for by users. Based on the title and site design, only Italian-speaking users. I wrote a custom web crawler to scrape the lyrics, which had difficulties since after a certain number of requests my IP address was banned. I changed strategies, and sampled only the first song from each album of the top 100 artists, and increased the delay between accesses to once every 2 seconds. My IP address was still banned three more times, but I collected data on 736 albums, resulting in 668 lyric texts.
+To evaluate the popularity of rap in Italy over time, I used as a proxy the publication date of songs found on the Italian lyrics website "aztesti.it". Although the process used to select songs is unknown, such websites are designed to optimize traffic, and so they offer a sample of songs regularly searched for by current users. Based on the title and site design, only Italian-speaking users. I wrote a custom web crawler to scrape the lyrics. This process ran into difficulties when, after a certain number of requests, my IP address was banned. I changed strategies, and sampled only the first song from each album of the top 100 artists, and increased the delay between accesses to once every 2 seconds. My IP address was still banned three more times, but I collected one song from 687 albums.
+
+I built a crude genre classifier to judge whether a song was a rap song or not, based on the total number of words, with the cutoff being 500 words. If the first song on an album was a rap song, that album was categorized as a rap album. Then I plotted the number of rap albums per year in the lyric database, along with the number of non-rap songs. 
 
 ![Plot of rap content by release year](RapItalia_plot_1.png)
 
-I built a crude genre classifier to judge whether a song was a rap song or not, based on the total number of words, with the cutoff being 500 words. If the first song on an album was a rap song, that album was categorized as a rap album. Then I plotted the number of rap albums per year in the lyric database, along with the number of non-rap songs. There was some support for my hypothesis, since most of the rap albums in the database were from the last 5 years. However, if anything, the proportion of rap albums went down over the last ten years.
+There was some support for my hypothesis, since most of the rap albums in the database were from the last 10 years. Even when viewed as a proportion, though, there does not seem to be a consistent trend in the proportion of rap albums:
+
+![Plot of rap content by release year (proportion)](RapItalia_plot_1a.png)
+
+As a secondary investigation, I used a pre-written language classifier (based on stopwords in the NTLK package) on the first songs, to see whether English-language albums were getting more popular with time relative to Italian albums, as another example of americanization of taste. The raw number of albums:
 
 ![Plot of language content by release year](RapItalia_plot_2.png)
 
-I also used a premade language classifier on the first songs, to see whether English-language albums were getting more popular with time relative to Italian albums, as another example of americanization of taste. There wasn't clear evidence for this, since there seem to be roughly equal numbers of English and Italian songs at the different years. 
+There are more English-language albums from recent years than Italian albums, and the trend is confirmed by the proportions:
 
-Although a larger data set, especially one with genre tags, will give more accurate measures, to track cool - and especially to forecast it - messy, non-traditional data sources need to be exploited. 
+![Plot of language content by release year (proportion)](RapItalia_plot_2a.png)
+
+English albums become more represented relative to Italian albums as the publication dates come closer to the present, at least with more consistent growth in the last 10 years (which are also less volatile proportions because of the larger samples).
+
+Although a larger and more systematically sampled data set, especially one with genre tags, will give more accurate measures, to track cool - and especially to forecast it - messy, non-traditional data sources need to be exploited. 
--- RapItalia.py
+++ RapItalia.py
@@ -3,14 +3,14 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 import codecs
+from nltk.corpus import stopwords
+import nltk.tokenize
 #%%
 ###############
 # The following code is copied from 
 # http://www.mostlymaths.net/2012/06/language-detection-in-python-with-nltk.html
 # And is written by Ruben Berenguel
 
-from nltk.corpus import stopwords
-import nltk.tokenize
 
 def scoreFunction(wholetext):
     """Get text, find most common words and compare with known
@@ -26,7 +26,6 @@ def scoreFunction(wholetext):
 "french","hungarian","norwegian","russian","swedish"]
     # Just in case I add stopword lists
 
-    FREElanguages=[""]
     languages=NLTKlanguages #+FREElanguages
     # Fill the dictionary of languages, to avoid  unnecessary function calls
     for lang in NLTKlanguages:
@@ -81,10 +80,12 @@ for i,songtitle in enumerate(first_song_info.song):
     try:
         with codecs.open(filename,'r','utf-8') as f:
             songtext = f.read()
-        
+            f.close()
     except IOError:
-        print str(i) + songtitle
+        print filename
         continue
+    
+    
     songtext = songtext.strip()
     re.sub('\r\n(\r\n)+','\r\n',songtext)
     
@@ -97,34 +98,37 @@ for i,songtitle in enumerate(first_song_info.song):
     except UnicodeDecodeError:
         first_song_info.loc[i,'language'] =np.nan
         
-
+#%%
 # Strip out songs without enough lyrics 
 song_valid = np.all([np.invert(first_song_info.word_count.isnull()),first_song_info.word_count > 25],0)
 first_song_info = first_song_info.loc[song_valid,:]
 
+##%%
+## Explore possible features for classifying rap songs 
+#words_per_line = first_song_info.loc[:,'word_count']/first_song_info.loc[:,'num_lines']
+#plt.figure()
+#plt.hist(words_per_line,bins=np.arange(2,10,0.2))
+#
+#plt.figure()
+#plt.hist(first_song_info.word_count,bins=np.arange(0,800,20))
+#
+#plt.figure()
+#plt.plot(first_song_info.word_count,words_per_line,'.')
+#
+#plt.figure()
+#plt.hist(first_song_info.avg_word_length,50)
+
 #%%
-# Explore possible features for classifying rap songs 
-words_per_line = first_song_info.loc[:,'word_count']/first_song_info.loc[:,'num_lines']
-plt.figure()
-plt.hist(words_per_line,bins=np.arange(2,10,0.2))
+# Compute rap content by cutoff and print detected song titles to check
 
-plt.figure()
-plt.hist(first_song_info.word_count,bins=np.arange(0,800,20))
-
-plt.figure()
-plt.plot(first_song_info.word_count,words_per_line,'.')
-
-plt.figure()
-plt.hist(first_song_info.avg_word_length,50)
-#%%
-# Check my rap cutoff by song title
 rapCutoff = 500
 
 first_song_info.loc[:,'is_rap'] = first_song_info.loc[:,'word_count'] > rapCutoff
-print first_song_info.loc[first_song_info.is_rap,['word_count','song']].sort_values(by='word_count')
+print "These songs were identified as rap: "
+print first_song_info.loc[first_song_info.is_rap,['song','word_count']].sort_values(by='word_count')
 
 #%%
-#Growth of rap by year
+# Rap albums: absolute
 
 byyear = first_song_info.groupby(by=['year'])
 totalalbums = byyear['song'].count()
@@ -133,9 +137,9 @@ nonrap = totalalbums-totalrap
 ind = totalalbums.index    # the x locations for the groups
 width = 0.45 
 plt.figure(figsize=[10,6])
-colours = [[0.3, 0.3, 0.8],[0, 0, 0]]
-p1 = plt.bar(ind-width, nonrap, width, color=colours[0],linewidth=0)
-p2 = plt.bar(ind, totalrap, width, color=colours[1],linewidth=0)
+rapcolours = [[0.5, 0.5, 1],[0, 0, 0]]
+p1 = plt.bar(ind-width, nonrap, width, color=rapcolours[0],linewidth=0)
+p2 = plt.bar(ind, totalrap, width, color=rapcolours[1],linewidth=0)
 plt.xlim([1965,2017])
 plt.xticks(range(1965,2020,5))
 format = 'png'
@@ -145,14 +149,37 @@ ax = plt.gca()
 ax.get_yaxis().set_tick_params(direction='in')
 ax.get_xaxis().set_tick_params(direction='in')
 plt.draw()
-plt.legend(['Non-rap','Rap'],loc='topleft')
-plt.title('Genre Content of Albums in an Italian Lyrics Database')
+plt.legend(['Non-rap','Rap'],loc='upper left')
+plt.title('Rap Content of Albums in an Italian Lyrics Database')
 
 
-plt.savefig('RapItalia_plot_1.png', dpi=200,format=format,bbox_inches="tight")
+plt.savefig('RapItalia_plot_1.png', dpi=100,format=format,bbox_inches="tight")
 
 #%%
-#Growth of english
+# Rap albums: proportions
+
+totalalbums = totalrap + nonrap
+rapprops = np.array([totalrap/totalalbums,nonrap/totalalbums])
+
+width = 0.7
+plt.figure(figsize=[10,6])
+lefts = ind-(0.5*width)
+p1 = plt.bar(lefts, rapprops[0,:], width, color=rapcolours[0],linewidth=0)
+p2 = plt.bar(lefts, rapprops[1,:], width, color=rapcolours[1], bottom=rapprops[0,:], linewidth=0)
+plt.ylabel('Proportion of albums')
+plt.xlabel('Release year')
+plt.legend(['Rap','Non Rap'],loc='upper left')
+plt.title('Rap Content of Albums in an Italian Lyrics Database (by Proportion)')
+
+plt.xticks(range(1965,2020,5))
+plt.xlim([1965,2017])
+
+plt.savefig('RapItalia_plot_1a.png', dpi=100,format=format,bbox_inches="tight")
+
+#%%
+# Languages: absolute numbers
+
+langcolours = np.array([[0.65,0.38,0.72],[0.3, 0.9, 0.27],[0.9,0.20,0.20]])
 
 byyearlang = first_song_info.groupby(by=['year','language'])
 languagereport = byyearlang['album'].count().unstack()
@@ -162,21 +189,41 @@ totalitalian = np.nan_to_num(languagereport.loc[:,'italian'])
 totalenglish = np.nan_to_num(languagereport.loc[:,'english'])
 totalother = np.nan_to_num(languagereport.loc[:,'other'])
 width =0.3
+
 plt.figure(figsize=[10,6])
-p1 = plt.bar(years-(width+0.5*width), totalenglish, width, color=[0.3, 0.9, 0.27],linewidth=0)
-p2 = plt.bar(years-(0.5*width), totalitalian, width, color=[0.65,0.38,0.72],linewidth=0)
-p3 = plt.bar(years+(width-0.5*width), totalother, width, color=[0.9,0.20,0.20],linewidth=0)
+p1 = plt.bar(years-(width+0.5*width), totalitalian, width, color=langcolours[0,:],linewidth=0)
+p2 = plt.bar(years-(0.5*width), totalenglish, width, color=langcolours[1,:],linewidth=0)
+p3 = plt.bar(years+(width-0.5*width), totalother, width, color=langcolours[2,:],linewidth=0)
 plt.ylabel('Albums')
 plt.xlabel('Release year')
 
 plt.xlim([1965,2017])
 plt.xticks(range(1965,2020,5))
-plt.legend(['English','Italian','Other'],title='Album language',loc='topleft')
+plt.legend(['Italian','English','Other'],title='Album language',loc='upper left')
 plt.title('Language Content of Albums in an Italian Lyrics Database')
 ax = plt.gca()
 ax.get_yaxis().set_tick_params(direction='in')
 ax.get_xaxis().set_tick_params(direction='in')
 plt.draw()
 
-plt.savefig('RapItalia_plot_2.png', dpi=200,format=format,bbox_inches="tight")
+plt.savefig('RapItalia_plot_2.png', dpi=100,format=format,bbox_inches="tight")
+#%%
+# Languages: proportions
+totalalbums = totalitalian + totalenglish + totalother
+langprops = np.array([totalitalian/totalalbums,totalenglish/totalalbums,totalother/totalalbums])
 
+width =0.7
+plt.figure(figsize=[10,6])
+lefts = years-(0.5*width)
+p1 = plt.bar(lefts, langprops[0,:], width, color=langcolours[0,:],linewidth=0)
+p2 = plt.bar(lefts, langprops[1,:], width, color=langcolours[1,:], bottom=langprops[0,:], linewidth=0)
+p3 = plt.bar(lefts, langprops[2,:], width, color=langcolours[2,:], bottom=(langprops[1,:]+langprops[0,:]),linewidth=0)
+plt.ylabel('Proportion of albums')
+plt.xlabel('Release year')
+plt.legend(['Italian','English','Other'],title='Album language',loc='lower left')
+plt.title('Language Content of Albums in an Italian Lyrics Database (by Proportion)')
+
+plt.xlim([1965,2017])
+plt.xticks(range(1965,2020,5))
+
+plt.savefig('RapItalia_plot_2a.png', dpi=100,format=format,bbox_inches="tight")
